52dcdd6ea85a89773dbd0a7c2f24751d2ac97ba1
com.AlifKhosyi <alifkhosyi@gmail.com>
2021-12-12 23:03:36 +0800

    Initial Commit

---
+++ CoffeeShop/CoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopApp.swift
+//  CoffeeShop
+//
+//  Created by Alif Khosyi Rahmatullah on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeShop
+//
+//  Created by Alif Khosyi Rahmatullah on 12/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
